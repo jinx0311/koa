be2e8d2bc9fc2b96f3d79ede171bc4c91cff1f90
wanglina <771922509@qq.com>
2021-11-09 11:35:42 +0800

    bug

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -2,12 +2,7 @@ class UserController {
     async register(ctx,next){
         ctx.body='用户注册成功'
     }
-    async login(ctx,next){
-        //1.获取数据
-        console.log(ctx.request.body);
-        //2.操作数据库
-
-        //3.返回结果
+    async login(ctx,next){ 
         ctx.body=ctx.request.body
     }
 }
